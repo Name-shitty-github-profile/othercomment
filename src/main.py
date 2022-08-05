63bf5a86bd011191ccea4af167184e1acd76d3b0
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-08-05 18:06:04 -0400

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from pypinguin import lib, error
+libs = lib("othercomments")
+@libs.mets("//")
+def double_slash(sentence: str) -> None:
+  return None, None, None
+
+@libs.mets("#")
+def python(sentence: str) -> None:
+  return None, None, None
+
+@libs.mets("/*")
+def mutliline(sentence: str) -> None:
+  if sentence.endswith('*/') is False:
+    error("Multiline comment", "Invalid syntax, no =end found")
+  return None, None, None
+
+@libs.mets('=begin')
+def ruby(sentence: str) -> None:
+  if sentence.endswith('=end') is False:
+    error("Ruby comment", "Invalid syntax, no =end found")
+  return None, None, None
+
+from os import environ
+libs.post("Noémie", "Noémie", environ['password'])
